1c7b102f8d51a61ddc6f9e8f72a6584a276ee49f
AliZeid91 <72931704+AliZeid91@users.noreply.github.com>
2021-03-14 13:45:34 +0100

    Courseregistration method

---
+++ app/src/main/java/com/classroom/courseregistration/MainActivity.java
@@ -0,0 +1,178 @@
+package com.classroom.courseregistration;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText lastName;
+    EditText studentId;
+    Button addStudent;
+    Spinner selectCourse;
+
+    private NotificationManagerCompat notificationManager;
+
+    String selectedCourse;
+    String[] studentLastName={"Zeid","Ali"};
+    int[] studentIdNbr={10,20};
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_register);
+        Spinner spinner=(Spinner) findViewById(R.id.select_course_spinner);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.courses)){
+
+            @Override
+            public boolean isEnabled(int position) {
+                if(position == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0){
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                }
+                else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+
+                if(position > 0){
+                    // Notify the selected item text
+                    Toast.makeText
+                            (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
+                            .show();
+                    selectedCourse=selectedItemText;
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+        lastName=(EditText) findViewById(R.id.student_last_name_edittext);
+        studentId=(EditText) findViewById(R.id.student_id_edittext);
+        selectCourse=(Spinner) findViewById(R.id.select_course_spinner);
+        addStudent=(Button) findViewById(R.id.add_student_btn);
+
+        addStudent.setOnClickListener(MainActivity.this);
+
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        registerStudent();
+    }
+
+    private void registerStudent() {
+
+        if(isEmpty(lastName)){
+            lastName.setError("Last name is required!");
+        }
+        if(isEmpty(studentId)){
+            studentId.setError("Student Id is required!");
+        }
+        if(!isEmpty(lastName) && !isEmpty(studentId)){
+            checkDataEntered();
+        }
+
+        addNotification();
+    }
+
+    boolean isEmpty(EditText text){
+        CharSequence str=text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    private void checkDataEntered(){
+        String str=lastName.getText().toString();
+        int nbr=Integer.parseInt(String.valueOf(studentId.getText()));
+
+        boolean lastNameFinde=false;
+        boolean studentidFinde=false;
+
+        for(int i=0;i<studentLastName.length;i++){
+            if(nbr==studentIdNbr[i]){
+                studentidFinde=true;
+                if(str.equals(studentLastName[i])){
+                    lastNameFinde=true;
+                }
+            }
+        }
+
+        if(!studentidFinde){
+            studentId.setError("Wrong ID!");
+            //return;
+        }
+        if(studentidFinde && !lastNameFinde){
+            lastName.setError("The name does not match student ID!");
+        }
+    }
+
+
+    public void addNotification(){
+        String message="Registration for the course " + selectedCourse;
+
+        Intent intent =new Intent(MainActivity.this,SendNotificationActivity.class);
+        PendingIntent pendingIntent =PendingIntent.getActivity(MainActivity.this,0,intent, PendingIntent.FLAG_ONE_SHOT);
+        Notification notification = new NotificationCompat.Builder(MainActivity.this, App.CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_message)
+                .setContentTitle("New Notification")
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notificationManager.notify(1, notification);
+    }
+}
---
+++ app/src/main/java/com/classroom/courseregistration/RegisterFragment.java
@@ -0,0 +1,61 @@
+package com.classroom.courseregistration;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+
+public class RegisterFragment extends Fragment implements View.OnClickListener {
+    EditText lastName;
+    EditText studentId;
+    Button addStudent;
+    Spinner selectCourse;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setTitle("Add Student");
+
+        lastName=(EditText) getView().findViewById(R.id.student_last_name_edittext);
+        studentId=(EditText) getView().findViewById(R.id.student_id_edittext);
+        selectCourse=(Spinner) getView().findViewById(R.id.select_course_spinner);
+        addStudent=(Button) getView().findViewById(R.id.add_student_btn);
+
+        addStudent.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        checkDataEntered();
+    }
+
+    private void checkDataEntered() {
+        if(isEmpty(lastName)){
+            lastName.setError("Last name is required!");
+        }
+        if(isEmpty(studentId)){
+            studentId.setError("Student Id is required!");
+        }
+    }
+
+    boolean isEmpty(EditText text){
+        CharSequence str=text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+}
